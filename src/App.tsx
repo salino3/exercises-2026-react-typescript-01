e57cd0fd84b3876c61dae8f2cc7e129505f37acd
Salino3 <flaviobcn012@gmail.com>
2026-01-10 18:41:41 +0200

    create first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Exercise01 } from "./components";
 import "./App.scss";
 
 function App() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <details>
+        <summary>Exercise 01 TS</summary>
+        <Exercise01 />
+      </details>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ex-01/ex-01.tsx
@@ -0,0 +1,6 @@
+import type React from "react";
+import "./exercise01.styles.scss";
+
+export const Exercise01: React.FC = () => {
+  return <div className="exercise01">Exercise01</div>;
+};
