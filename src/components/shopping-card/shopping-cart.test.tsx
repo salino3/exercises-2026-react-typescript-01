4056322666036826842dc9a72e8418e955711f5f
Salino3 <flaviobcn012@gmail.com>
2026-01-25 15:23:26 +0200

    add test 2

--- src/components/shopping-card/shopping-cart.test.tsx
+++ src/components/shopping-card/shopping-cart.test.tsx
@@ -7,18 +7,29 @@ import "@testing-library/jest-dom/vitest";
 describe("ShoppingCart", () => {
   it("clicking to Laptop Pro, I visualize one Laptop Pro in the cart list", async () => {
     const user = userEvent.setup();
-    const mockList = availableProducts;
     const { container } = render(<ShoppingCart />);
 
     const btn = container.querySelector('[data-btn-id="btn-Laptop Pro"]');
-
-    console.log("clog1", btn);
 
     if (!btn) return new Error("Button didn't find");
 
     await user.click(btn);
     const listCart = await screen.findByTestId("containerProducts");
+    const itemCart = await within(listCart).findByText(/Laptop Pro/i);
 
-    expect(within(listCart).findByText(/Laptop Pro/i));
+    expect(itemCart).toBeInTheDocument();
+  });
+
+  it("Appear 3 available products in the list", () => {
+    const { container } = render(<ShoppingCart />);
+
+    const availableProductsElements =
+      container.querySelectorAll(".cardProducts");
+
+    expect(availableProductsElements.length).toBe(3);
+
+    expect(screen.getByText(/Laptop Pro/i)).toBeInTheDocument();
+    expect(screen.getByText(/Camiseta Algodón/i)).toBeInTheDocument();
+    expect(screen.getByText(/Cafetera/i)).toBeInTheDocument();
   });
 });
